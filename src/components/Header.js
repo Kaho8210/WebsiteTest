43603e83b3b5b566e9767389ab30bfcfe7bb6e61
Kaho8210 <s1280204@u-aizu.ac.jp>
2023-01-10 17:20:51 +0900

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <div classname="logo">
+            <h3>Portofolio</h3>
+        </div>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">HOME</a>
+                </li>
+                <li>
+                    <a href="#">ABOUT MYSELF</a>
+                </li>
+                <li>
+                    <a href="#">ARTS</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
